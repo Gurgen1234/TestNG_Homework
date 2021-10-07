facd4a6dd25bf7d28755e40f1c013291e94f2b32
Gurgen1234 <goga74543@gmail.com>
2021-10-08 00:58:02 +0300

    First commir

---
+++ src/main/java/ru/lanit/testng/Calc.java
@@ -0,0 +1,15 @@
+package ru.lanit.testng;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String firstNum =  in.next();
+        String oper =  in.next();
+        String secondNum =  in.next();
+        Counter a = new Counter();
+        System.out.println(a.start(firstNum, oper, secondNum));
+    }
+}
